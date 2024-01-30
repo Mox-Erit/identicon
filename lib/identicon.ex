82326d00cbce31785d4ba0025b378b279892b551
Chase <calber2017@gmail.com>
2024-01-30 11:44:55 -0700

    added help function for chunking output and mirroring the lists

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,10 +3,7 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
-  end
-
-  def pick_color(%Identicon.Image{hex: [r, g, b | _tail] } = image) do
-    %Identicon.Image{image | color: {r, g, b} }
+    |> build_grid
   end
 
   def hash_input(input) do
@@ -15,4 +12,23 @@ defmodule Identicon do
 
     %Identicon.Image{hex: hex}
   end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail] } = image) do
+    %Identicon.Image{image | color: {r, g, b} }
+  end
+
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+    |> Enum.map(&mirror_row/1)
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+
+    row ++ [second, first]
+  end
+
+
 end
